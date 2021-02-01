2a3e81e429432c3e745c317ffc817e7ca5e836f2
Devin Rasmussen <devin.rasmussen@getcanopy.com>
2021-01-31 22:23:35 -0700

    introduce convert-to-roman tests

---
+++ functions/convert-to-roman.js
@@ -0,0 +1,36 @@
+const numbers = [1000, 500, 400, 100, 50, 40, 10, 5]
+
+function convertToRoman(int) {
+  var romanRef = {
+    // https://en.wiktionary.org/wiki/Appendix:Latin_cardinal_numerals
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  return Object.keys(romanRef).reduce((acc, key) => {
+    // loop through all roman numerals starting from the highest (M)
+    // grab the number equivalent
+    const val = romanRef[key];
+    // determine the floored quotient of provided int divided by current roman-equivalent number
+    // this represents how many times (if any) a particular roman numeral should be repeated
+    const quantity = Math.floor(int / val);
+
+    // reduce the provided int and concatenate roman numeral symbols
+    int -= quantity * val;
+    return acc + key.repeat(quantity);
+  }, '');
+}
+
+module.exports = {
+  convertToRoman
+}
