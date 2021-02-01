155eb8d2e96b219f3f777ef6131e396eb503be0b
Devin Rasmussen <devin.rasmussen@getcanopy.com>
2021-02-01 08:16:18 -0700

    include initial approach

--- functions/convert-to-roman.js
+++ functions/convert-to-roman.js
@@ -32,3 +32,55 @@ function convertToRoman(int) {
 module.exports = {
   convertToRoman
 }
+
+// initial approach included for interview discussion
+// recursion on substring
+
+// const romanRef = {
+//   1: 'I',
+//   5: 'V',
+//   10: 'X',
+//   50: 'L',
+//   100: 'C',
+//   500: 'D',
+//   1000: 'M'
+// };
+
+// export const convertToRoman = int => {
+//   const parsed = parseInt(int) // parseInt here takes '03' (from say 103) to 3
+//   const numbers = Object.keys(romanRef);
+//   let nextHighest, nextLowest
+//   numbers.some((num, index) => {
+//     if (num >= parsed) {
+//       nextHighest = num;
+//       nextLowest = numbers[index - 1];
+//       return true;
+//     }
+//   })
+//   if (!nextHighest) { // over 1000
+//     nextLowest = '1000'
+//   }
+//   // the equality operator is used here (rather than identity) because the nextHighest/nextLowest are strings from object.keys()
+//   if (nextHighest == parsed) return romanRef[nextHighest]; // 1 is 1
+//   const sub = `${parsed}`.substring('1');
+//   return `${repeatQuantity(parsed, nextLowest, nextHighest)}${parseInt(sub) ? convertToRoman(sub) : ''}`; // sub might be 0 (e.g., 40)
+// };
+
+// const repeatQuantity = (int, low, high) => {
+//   const one = low.includes('1')
+//   const quantity = one
+//     ? Math.floor(int / low) // 42/10 -> 4
+//     : `${int}`[0] - low[0]; // 6-5
+//   if (quantity < 4) { // 3
+//     return one
+//       ? romanRef[low].repeat(quantity) // III
+//       : `${romanRef[low]}${romanRef[low.replace('5', '1')].repeat(quantity)}` // VII
+//   } else { // 40 is a good example or 90
+//     const countFromHigher = one
+//       ? high[0] - `${int}`[0] // 50 - 40 gives us 1
+//       : 10 - `${int}`[0] // 10 - 9 gives us 1
+//     return one
+//       ? `${romanRef[low].repeat(countFromHigher)}${romanRef[high]}` // IV
+//       : `${romanRef[low.replace('5', '1')].repeat(countFromHigher)}${romanRef[high]}` // IX
+//   }
+// }
