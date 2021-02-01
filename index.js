d775b13b4dcfb6674b0db1fbb6117bdeb4f552ff
Devin Rasmussen <devin.rasmussen@getcanopy.com>
2021-01-31 22:24:03 -0700

    make index.js easier to read by renaming provided query variable

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express')
-const { isPositiveInt } = require('./validate')
+const { convertToRoman } = require('./functions/convert-to-roman')
+const { isPositiveInt } = require('./functions/validate')
 const app = express()
 
 app.get('/romannumeral', (req, res) => {
   let error
-  if (!req.query.query) {
-    error = "No value was provided in the query component: ?query={integer}"
-  } else if (!isPositiveInt(req.query.query)) {
-    error = `'${req.query.query}' is not a valid positive integer`
+  const {query: providedValue} = req.query
+  if (!providedValue) {
+    error = "No value was provided on the expected query parameter: ?query={integer}";
+  } else if (!isPositiveInt(providedValue)) {
+    error = `'${providedValue}' is not a valid positive integer`;
   }
   if (error) {
     res.status(400).send(error)
   } else {
-    res.send('XV')
+    res.send(convertToRoman(providedValue))
   }
 })
 
